b1b05d880e1477801db1dec398ea768c5764935d
garecinos4 <recinos.alejandra@gmail.com>
2015-02-22 18:51:44 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 install.packages("dplyr")
 library(dplyr)
 
-Step1
+##Step1: Merges the training and the test sets to create one data set.
 
 train.temp1 <- read.table("train/X_train.txt")
 test.temp1 <- read.table("test/X_test.txt")
@@ -20,23 +20,25 @@ y <- rbind(train.temp3, test.temp3)
 
 data <- cbind(x, y, subject)
 
-Step 2
+##Step 2 Extracts only the measurements on the mean and standard deviation for each measurement.
 data_features<- read.table("features.txt")
 
 features<-data_features[grep("mean\\(\\)|std\\(\\)", data_features$V2),]
 features.data <- x[,features$V1]
 
-Step 3
+##Step 3 Uses descriptive activity names to name the activities in the data set
 data_activities <- read.table("activity_labels.txt")
  
 activities <- inner_join(y, data_activities, by="V1")
 
 activities.data <- activities$V2
-Step 4
+
+##Step 4 Appropriately labels the data set with descriptive variable names. 
 data <- cbind(features.data , activities.data, subject )
 names(data) <- c(as.character(features$V2), "activity", "subject");
 
-Step 5
+##Step 5 From the data set in step 4, creates a second, independent tidy data set with the 
+##average of each variable for each activity and each subject.
 
 data2 <- data;
 final_data <- data2 %>% group_by(activity,subject) %>% summarise_each(funs(mean))
